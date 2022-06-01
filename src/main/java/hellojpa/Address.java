16f78d36cd4b4c182628d412cc110df296881fda
lalalalz <lalalalz@naver.com>
2022-06-01 21:43:27 +0900

    #ADD
    1. 주소 엔티티 추가

--- src/main/java/hellojpa/Address.java
+++ src/main/java/hellojpa/Address.java
@@ -5,11 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Member {
+public class Address {
 
     @Id @GeneratedValue
     private Long id;
-    private String name;
-    private Integer age;
-    private Address address;
 }
